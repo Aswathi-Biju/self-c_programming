439ce7d8856e1ddbda74a78bd182969ccd9a6443
Aswathi Biju <aswathibiju8118@gmail.com>
2025-01-23 23:33:06 +0530

    Create arithmetic.c

---
+++ arithmetic.c
@@ -0,0 +1,16 @@
+// Arthmetic operation(+,-,*,/):
+#include<stdio.h>
+int main() {
+    int number1= 9;
+    int number2= 5;
+    int sum, diff, multiplication, division;
+    sum= number1+number2;
+    diff= number1-number2;
+    multiplication= number1*number2;
+    division= number1/number2;
+    printf("The sum of two integers are:%d \n", sum);
+    printf("The difference of two integers are:%d \n", diff);
+    printf("The multiplication of two integers are:%d \n", multiplication);
+    printf("The division of two integers are:%d \n", division);
+    return 0;
+}
