21d43abb3dabcc3da9aa103a5678c8839b93e6de
Aswathi Biju <aswathibiju8118@gmail.com>
2025-02-17 22:10:42 +0530

    Add files via upload

---
+++ traces.c
@@ -0,0 +1,35 @@
+#include<stdio.h>
+int main(){
+    int row,col,i,j,sum=0;
+    printf("Enter a no. for row and coloumn:\n");
+    scanf("%d\n%d", &row,&col);
+    int matrix[row][col];
+    if(row==col){
+        printf("The matrix is a suqare matrix\n");
+        printf("Enter element values for matrix:\n");
+        for (i=0;i<row;i++){
+            for(j=0;j<col;j++){
+                scanf("%d", &matrix[i][j]);   
+            }
+        }
+        printf("The elements for matrix:\n");
+        for (i=0;i<row;i++){
+            for(j=0;j<col;j++){
+                printf("%d\t", matrix[i][j]);   
+            }
+            printf("\n");
+        }
+        for (i=0;i<row;i++){
+            for(j=0;j<col;j++){
+                if(i==j){
+                    sum=sum+matrix[i][j];
+                }
+            }
+        }
+        printf("The sum of the matrix is: ");
+        printf("%d",sum);
+    }else{
+        printf("The matrix is not a square matrix, so can't calculate the trace.\n");
+    }
+    return 0;
+}
